ab9ebce2b4d497fed35938bbdc3a2350a8a20b57
Samuelpeter001 <samuelpeter61@gmail.com>
2025-01-19 21:57:50 +0100

    updated assignment

---
+++ scripts/getdates.js
@@ -0,0 +1,3 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('copyright').textContent = `© ${currentYear} Samuel Peter`;
+document.getElementById('last-modified').textContent = `Last modified: ${document.lastModified}`;
